13817cb7563609e5a5b3442550872624b703a1d2
EthanCrowe <voronyk0304@gmail.com>
2024-10-06 15:31:00 -0400

    dict code added

--- lab_1/calc_entropy/dict.c
+++ lab_1/calc_entropy/dict.c
@@ -1,1 +1,63 @@
+#include "dict.h"
+
 #include <wchar.h>
+#include <stdlib.h>
+#include <assert.h>
+
+Dict *Dict_create(uint32 size) {
+    assert(size > 0 && "Negative size of dictionary");
+
+    Dict *dict = (Dict *) malloc(sizeof(Dict));
+    dict->keys = (dict_key_t *) malloc(sizeof(dict_key_t) * size);
+    dict->values = (dict_value_t *) malloc(sizeof(dict_value_t) * size);
+    dict->size = size;
+    dict->occupied = 0;
+
+    return dict;
+}
+
+
+void Dict_destroy(Dict *dict) {
+    assert(NULL != dict);
+    
+    free(dict->keys);
+    free(dict->values);
+    free(dict);
+}
+
+
+dict_value_t Dict_get(Dict *dict, dict_key_t key) {
+    int i;
+
+    for (i = 0; i < dict->size; ++i) {
+        if(wcsstr(key, dict->keys[i])) {
+            return dict->values[i];
+        }
+    }
+
+    return VALUE_NOT_EXIST;
+} 
+
+
+void Dict_insert(Dict *dict, dict_key_t key, dict_value_t value) {
+    assert(Dict_is_full(dict));
+    
+    dict->keys[dict->occupied] = key;
+    dict->values[dict->occupied] = value;
+    dict->occupied++;
+}
+
+//??????
+void Dict_remove(Dict dict, dict_key_t key) {
+
+}
+
+
+bool Dict_is_empty(Dict *dict) {
+    return dict->occupied == dict->size;
+}
+
+
+bool Dict_is_full(Dict *dict) {
+    return dict->occupied == 0;
+}
--- lab_1/calc_entropy/dict.h
+++ lab_1/calc_entropy/dict.h
@@ -2,26 +2,36 @@
 #define DICT_H
 
 #include <wchar.h>
+#include <stdbool.h>
 
 typedef unsigned int uint32;
 typedef wchar_t * dict_key_t;
 typedef uint32 dict_value_t;
 
+enum {
+    VALUE_NOT_EXIST = -1,
+};
 
 struct s_dict {
     dict_key_t      *keys;
     dict_value_t    *values;
+
+    uint32          occupied;
     uint32          size;
 };
 
 
-typedef s_dict Dict;
+typedef struct s_dict Dict;
 
 Dict *Dict_create(uint32 size);
 void Dict_destroy(Dict *dict);
 
 dict_value_t Dict_get(Dict *dict, dict_key_t key);
-void Dict_insert(Dict dict, dict_key_t key, dict_value_t value);
-void Dict_remove(Dict dict, dict_key_t key);
+void Dict_insert(Dict *dict, dict_key_t key, dict_value_t value);
+void Dict_remove(Dict *dict, dict_key_t key);
+
+bool Dict_is_empty(Dict *dict);
+bool Dict_is_full(Dict *dict);
+
 
 #endif
